9a1c44c597bb144d1b37d12dadee481dc5c32171
kim <kimzero@hotmail.com>
2018-06-08 14:48:41 +0200

    No need to allow cross origin access

--- server.js
+++ server.js
@@ -26,13 +26,6 @@ const app = express();
  */
 app.use(bodyParser.json());
 
-// Allow CORS
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 
 const UserSchema = mongoose.Schema({
   firstName: String,
